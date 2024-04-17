77f8866c86ebd03bb8b684a56bb67f2c0134dd63
Ryan Kienstra <ryan.kienstra@wpengine.com>
2024-04-17 10:33:42 -0600

    Make the <tr> elements dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,68 @@
 import './App.css'
 
 function App() {
+  const personSchema = [
+    {
+      slug: 'name',
+      title: 'Name',
+    },
+    {
+      slug: 'location',
+      title: 'Location',
+    },
+    {
+      slug: 'wld',
+      title: 'W-L-D',
+    },
+    {
+      slug: 'division',
+      title: 'Division',
+    },
+    {
+      slug: 'career',
+      title: 'Career',
+    },
+  ]
+
+  const people = [
+    {
+      name: 'Juan Garcia',
+      location: 'Morocco',
+      wld: '3-0-0',
+      division: 'light heavy',
+      career: '2020-2024'
+    }
+  ]
+
   return (
     <>
       <h1 className="text-4xl font-bold mb-4">People</h1>
       <table className="border-separate border-spacing-2 w-full border border-slate-400 bg-white text-sm shadow-sm">
         <thead className="bg-slate-50">
           <tr>
-            <th className="w-1/5 border border-slate-300 font-semibold p-4 text-slate-900 text-left">Name</th>
-            <th className="w-1/5 border border-slate-300 font-semibold p-4 text-slate-900 text-left">Location</th>
-            <th className="w-1/5 border border-slate-300 font-semibold p-4 text-slate-900 text-left">W-L-D</th>
-            <th className="w-1/5 border border-slate-300 font-semibold p-4 text-slate-900 text-left">Division</th>
-            <th className="w-1/5 border border-slate-300 font-semibold p-4 text-slate-900 text-left">Career</th>
+            {personSchema.map(person => (
+              <th 
+                key={person.slug}
+                className="w-1/5 border border-slate-300 font-semibold p-4 text-slate-900 text-left"
+              >
+                {person.title}
+              </th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className="border border-slate-300 p-4 text-slate-500">John Smith</td>
-            <td className="border border-slate-300 p-4 text-slate-500">New Zealand</td>
-            <td className="border border-slate-300 p-4 text-slate-500">2 1 0</td>
-            <td className="border border-slate-300 p-4 text-slate-500">super light</td>
-            <td className="border border-slate-300 p-4 text-slate-500">2020-2023</td>
-          </tr>
-          <tr>
-            <td className="border border-slate-300 p-4 text-slate-500">Juan Garcia</td>
-            <td className="border border-slate-300 p-4 text-slate-500">Morocco</td>
-            <td className="border border-slate-300 p-4 text-slate-500">3 0 0</td>
-            <td className="border border-slate-300 p-4 text-slate-500">light heavy</td>
-            <td className="border border-slate-300 p-4 text-slate-500">2020-2024</td>
-          </tr>
-          <tr>
-            <td className="border border-slate-300 p-4 text-slate-500">Andre DePaul</td>
-            <td className="border border-slate-300 p-4 text-slate-500">Corsica</td>
-            <td className="border border-slate-300 p-4 text-slate-500">1 1 1</td>
-            <td className="border border-slate-300 p-4 text-slate-500">feather</td>
-            <td className="border border-slate-300 p-4 text-slate-500">2019-2022</td>
-          </tr>
+          {people.map(person => (
+            <tr key={person.name}>
+              {personSchema.map(s => (
+                <td
+                  key={s.slug}
+                  className="border border-slate-300 p-4 text-slate-500"
+                >
+                  {person[s.slug]}
+                </td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
     </>
