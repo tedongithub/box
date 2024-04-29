2c810a90a8bbb85863acf0473162c5f45dd05127
Ryan Kienstra <ryan.kienstra@wpengine.com>
2024-04-29 09:48:10 -0600

    Add a prettier setup, and format using prettier

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,10 @@ function Dropdown() {
       <div>
         <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
           Options
-          <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+          <ChevronDownIcon
+            className="-mr-1 h-5 w-5 text-gray-400"
+            aria-hidden="true"
+          />
         </Menu.Button>
       </div>
 
@@ -26,20 +29,18 @@ function Dropdown() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        { /* no w-56 */ }
+        {/* no w-56 */}
         <Menu.Items className="absolute right-0 z-10 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             <Menu.Item>
-              {({active}) => (
+              {({ active }) => (
                 <a
                   href="#"
                   className={classNames(
                     active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                     'block px-4 py-2 text-sm'
                   )}
-                  onClick={ () => {
-                    console.log('clicked')
-                  }}
+                  onClick={() =>{}}
                 >
                   Account settings
                 </a>
@@ -86,34 +87,74 @@ function App() {
       <table className="border-separate border-spacing-2 w-full border border-slate-400 bg-white text-sm shadow-sm">
         <thead className="bg-slate-50">
           <tr>
-            <th className="w-1/5 border border-slate-300 font-semibold p-4 text-slate-900 text-left">Name</th>
-            <th className="w-1/5 border border-slate-300 font-semibold p-4 text-slate-900 text-left">Location</th>
-            <th className="w-1/5 border border-slate-300 font-semibold p-4 text-slate-900 text-left">W-L-D</th>
-            <th className="w-1/5 border border-slate-300 font-semibold p-4 text-slate-900 text-left">Division</th>
-            <th className="w-1/5 border border-slate-300 font-semibold p-4 text-slate-900 text-left">Career</th>
+            <th className="w-1/5 border border-slate-300 font-semibold p-4 text-slate-900 text-left">
+              Name
+            </th>
+            <th className="w-1/5 border border-slate-300 font-semibold p-4 text-slate-900 text-left">
+              Location
+            </th>
+            <th className="w-1/5 border border-slate-300 font-semibold p-4 text-slate-900 text-left">
+              W-L-D
+            </th>
+            <th className="w-1/5 border border-slate-300 font-semibold p-4 text-slate-900 text-left">
+              Division
+            </th>
+            <th className="w-1/5 border border-slate-300 font-semibold p-4 text-slate-900 text-left">
+              Career
+            </th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td className="border border-slate-300 p-4 text-slate-500">John Smith</td>
-            <td className="border border-slate-300 p-4 text-slate-500">New Zealand</td>
-            <td className="border border-slate-300 p-4 text-slate-500">2 1 0</td>
-            <td className="border border-slate-300 p-4 text-slate-500">super light</td>
-            <td className="border border-slate-300 p-4 text-slate-500">2020-2023</td>
+            <td className="border border-slate-300 p-4 text-slate-500">
+              John Smith
+            </td>
+            <td className="border border-slate-300 p-4 text-slate-500">
+              New Zealand
+            </td>
+            <td className="border border-slate-300 p-4 text-slate-500">
+              2 1 0
+            </td>
+            <td className="border border-slate-300 p-4 text-slate-500">
+              super light
+            </td>
+            <td className="border border-slate-300 p-4 text-slate-500">
+              2020-2023
+            </td>
           </tr>
           <tr>
-            <td className="border border-slate-300 p-4 text-slate-500">Juan Garcia</td>
-            <td className="border border-slate-300 p-4 text-slate-500">Morocco</td>
-            <td className="border border-slate-300 p-4 text-slate-500">3 0 0</td>
-            <td className="border border-slate-300 p-4 text-slate-500">light heavy</td>
-            <td className="border border-slate-300 p-4 text-slate-500">2020-2024</td>
+            <td className="border border-slate-300 p-4 text-slate-500">
+              Juan Garcia
+            </td>
+            <td className="border border-slate-300 p-4 text-slate-500">
+              Morocco
+            </td>
+            <td className="border border-slate-300 p-4 text-slate-500">
+              3 0 0
+            </td>
+            <td className="border border-slate-300 p-4 text-slate-500">
+              light heavy
+            </td>
+            <td className="border border-slate-300 p-4 text-slate-500">
+              2020-2024
+            </td>
           </tr>
           <tr>
-            <td className="border border-slate-300 p-4 text-slate-500">Andre DePaul</td>
-            <td className="border border-slate-300 p-4 text-slate-500">Corsica</td>
-            <td className="border border-slate-300 p-4 text-slate-500">1 1 1</td>
-            <td className="border border-slate-300 p-4 text-slate-500">feather</td>
-            <td className="border border-slate-300 p-4 text-slate-500">2019-2022</td>
+            <td className="border border-slate-300 p-4 text-slate-500">
+              Andre DePaul
+            </td>
+            <td className="border border-slate-300 p-4 text-slate-500">
+              Corsica
+            </td>
+            <td className="border border-slate-300 p-4 text-slate-500">
+              1 1 1
+            </td>
+            <td className="border border-slate-300 p-4 text-slate-500">
+              feather
+            </td>
+            <td className="border border-slate-300 p-4 text-slate-500">
+              2019-2022
+            </td>
           </tr>
         </tbody>
       </table>
